7b85b0447637b9dbd7eb3c07c93fc484f22f734e
minthitsar125226 <140641275+minthitsar125226@users.noreply.github.com>
2026-03-12 01:02:29 +0630

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 import streamlit as st
-from utils import format_gold_weight, calculate_pe, to_pe, from_pe, mm_to_inch_pe, inch_pe_to_mm
-
+from datetime import datetime
+from utils import (
+    format_gold_weight, calculate_pe, 
+    mm_to_inch_pe, inch_pe_to_mm, format_length_inches
+)
 st.set_page_config(page_title="မြန်မာ့ရွှေပန်းတိမ်သုံး", page_icon="⚒️", layout="wide")
 
 # Theme CSS
