6826d61f1c01e4a0e3c0b21609ba5a0dff20602c
minthitsar125226 <140641275+minthitsar125226@users.noreply.github.com>
2026-03-13 01:32:39 +0630

    Update logic.py

--- logic.py
+++ logic.py
@@ -115,13 +115,14 @@ def calculate_gold_price_comprehensive(kyat, pae, yway, point, gold_price):
     return gold_cost
 
 def gem_to_gold_units(carat):
-    # 1 Carat လျှင် ရွှေချိန် Point ပေါင်း ၁၉.၂၆၄ ဝန်းကျင်ရှိသည်ဟု တွက်ချက်ထားခြင်း
-    total_gold_points = carat * 19.264
+    # 1 Carat လျှင် ရွှေချိန် Point ပေါင်း ၁၉.၂၆၄ (ခန့်မှန်းခြေ) ရှိသည်
+    # အလေးချိန် (Carat) အလိုက် ပြောင်းလဲအောင် total_gold_points ကို အောက်ပါအတိုင်း တွက်ပါ
+    total_gold_points = float(carat) * 19.264
     
     kyat = int(total_gold_points // 120)
     pae = int((total_gold_points % 120) // 7.5)
     yway = int((total_gold_points % 7.5) // 1)
+    # Point ကို ဓာတ်သမ ၁ နေရာအထိ ယူခြင်း
     point = round((total_gold_points % 1) * 10, 1)
     
-    # ဒီ return ပြန်တဲ့နေရာမှာ အောက်ပါအတိုင်း နာမည်တွေ အကုန်ပါရပါမယ်
     return {"kyat": kyat, "pae": pae, "yway": yway, "point": point}
